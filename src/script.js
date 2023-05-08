f7f56b0c5d1b44538ce0926400cc4ef204693dad
daryapatsko <darya.patsko@mail.ru>
2023-05-08 13:33:57 +0300

    fixed adaptiv style

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
     document.getElementById('burger').addEventListener('click',function(){
-        document.querySelector(".menu_header").classList.toggle("open"),
-        document.querySelector(".header").classList.toggle("open")
+        document.querySelector(".header_menu").classList.toggle("open"),
+        document.querySelector(".header_burger").classList.toggle("open"),
+        document.querySelector("body").classList.toggle("lock")
+
     })
 })
